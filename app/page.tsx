4c24bdf89e7ef39634c01a81330acc431d9877af
Upvisor <contacto.upvisor@gmail.com>
2024-01-18 16:57:29 -0300

    actualization home

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function page () {
         <title>Inicio</title>
       </Head>
       <div className='p-6 bg-[#f6f6f7] w-full h-full flex flex-col gap-4 dark:bg-neutral-900' style={{ overflow: 'overlay' }}>
-        <div className='flex justify-between w-full max-w-1280 mx-auto mb-4'>
+        <div className='flex justify-between w-full max-w-1280 mx-auto'>
           <h1 className='text-xl font-medium'>Inicio</h1>
           <Link className='pt-1.5 pb-1.5 h-fit pl-7 pr-7 rounded bg-main border border-main text-white hover:bg-transparent hover:text-main transition-colors duration-200' href='/ventas/nueva-venta'>Nueva venta</Link>
         </div>
