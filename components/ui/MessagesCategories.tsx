a38f3bb3ce7a2477caede5b8ccbd7ec7eb92c483
Upvisor <contacto.upvisor@gmail.com>
2024-03-31 18:16:46 -0300

    actualization genrela

--- components/ui/MessagesCategories.tsx
+++ components/ui/MessagesCategories.tsx
@@ -8,10 +8,10 @@ const pathname = usePathname()
 
   return (
     <div className='flex gap-3'>
-      <Link href='/mensajes' className={`${pathname === '/mensajes' ? 'bg-main text-white' : 'bg-white dark:bg-neutral-700/60 hover:bg-main/20'} p-2 rounded shadow transition-colors duration-200`}>Chatbot web</Link>
-      <Link href='/mensajes/whatsapp' className={`${pathname === '/mensajes/whatsapp' ? 'bg-main text-white' : 'bg-white dark:bg-neutral-700/60 hover:bg-main/20'} p-2 rounded shadow transition-colors duration-200`}>Whatsapp</Link>
-      <Link href='/mensajes/messenger' className={`${pathname === '/mensajes/messenger' ? 'bg-main text-white' : 'bg-white dark:bg-neutral-700/60 hover:bg-main/20'} p-2 rounded shadow transition-colors duration-200`}>Messenger</Link>
-      <Link href='/mensajes/instagram' className={`${pathname === '/mensajes/instagram' ? 'bg-main text-white' : 'bg-white dark:bg-neutral-700/60 hover:bg-main/20'} p-2 rounded shadow transition-colors duration-200`}>Instagram</Link>
+      <Link href='/mensajes' className={`${pathname === '/mensajes' ? 'bg-main text-white' : 'bg-white dark:bg-neutral-700/60 hover:bg-main/20 dark:hover:bg-main/20'} p-2 rounded shadow transition-colors duration-200`}>Chatbot web</Link>
+      <Link href='/mensajes/whatsapp' className={`${pathname === '/mensajes/whatsapp' ? 'bg-main text-white' : 'bg-white dark:bg-neutral-700/60 hover:bg-main/20 dark:hover:bg-main/20'} p-2 rounded shadow transition-colors duration-200`}>Whatsapp</Link>
+      <Link href='/mensajes/messenger' className={`${pathname === '/mensajes/messenger' ? 'bg-main text-white' : 'bg-white dark:bg-neutral-700/60 hover:bg-main/20 dark:hover:bg-main/20'} p-2 rounded shadow transition-colors duration-200`}>Messenger</Link>
+      <Link href='/mensajes/instagram' className={`${pathname === '/mensajes/instagram' ? 'bg-main text-white' : 'bg-white dark:bg-neutral-700/60 hover:bg-main/20 dark:hover:bg-main/20'} p-2 rounded shadow transition-colors duration-200`}>Instagram</Link>
     </div>
   )
 }
