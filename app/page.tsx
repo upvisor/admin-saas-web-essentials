87df43981ec4685590f4f1a97780961314e59555
Upvisor <contacto.upvisor@gmail.com>
2024-10-29 01:27:03 -0300

    actualization general

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,8 @@
 "use client"
-import Head from "next/head"
 
 export default function page () {
   return (
     <>
-      <Head>
-        <title>Inicio</title>
-      </Head>
       <div className='p-6 bg-bg w-full h-full flex flex-col gap-6 dark:bg-neutral-900' style={{ overflow: 'overlay' }}>
         <div className='flex justify-between w-full max-w-[1280px] mx-auto'>
           <h1 className='text-2xl my-auto font-medium'>Inicio</h1>
